6dac80a9fdfea061cb629a84c0864af4c1a5a7d1
Emub <sodavansjuice@gmail.com>
2013-08-25 16:37:25 +0200

    DJ Octavia made me change the permission on !skipthis to bouncer or higher <.<

--- mubBotUser.js
+++ mubBotUser.js
@@ -233,7 +233,7 @@ botMethods.chatEvent = function(data){
 				break;
 
 				case "skipthis":
-					API.getUser(data.fromID).permission > 1 ? API.moderateForceSkip() : API.sendChat("This commands requires being a room bouncer or of higher rank!");
+					API.getUser(data.fromID).permission > 1 ? API.moderateForceSkip() : API.sendChat("This commands requires being a room bouncer or of even higher rank!");
 				break;
 
 				case "temp":
