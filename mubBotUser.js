700b39e288d4837e81f5890b96053a37e9807641
derpthebass <ramboxp@live.com>
2013-09-02 16:06:33 -0400

    Update mubBotUser.js

--- mubBotUser.js
+++ mubBotUser.js
@@ -269,7 +269,7 @@ botMethods.chatEvent = function(data){
 			        	}
 			        	break;
 			            case "wiki":                                                                                          
-        				var r = data.message.split(6)[1].replace(" ", "_");                                                           
+        				var r = data.message.substring(6).replace(/ /g, "_")                                                          
         				API.sendChat("@"+data.from+" http://en.wikipedia.org/wiki/"+r+"(NOT GUARANTEED TO BE CORRECT)");   
 					break;                                                                                                    
 				case "linkify":
