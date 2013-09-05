29a70136f7f0a6bd333aef6db0836c014ccce760
derpthebass <ramboxp@live.com>
2013-09-04 22:48:49 -0400

    Update mubBotUser.js

--- mubBotUser.js
+++ mubBotUser.js
@@ -193,6 +193,8 @@ botMethods.chatEvent = function(data){
     command = false; var chatCommand = "";
     var permission = botMethods.getPermissions(data.fromID);
     if(data.message.indexOf("!") === 0) command = true;
+    emote = false; var chatCommand = "";
+    if(data.message.indexOf(":") === 0) emote = true;
     if(command){
 
         chatCommand = data.message.substring(1);
@@ -635,6 +637,22 @@ botMethods.chatEvent = function(data){
         }
         mubBot.misc.ready = false;
         setTimeout(function(){ mubBot.misc.ready = true; }, mubBot.settings.cooldown * 1000);
+    }else if(emote){
+        if(mubBot.misc.ready){
+            switch(chatCommand.substring(1)){
+                case "notamused:":
+                    API.sendChat("/me ಠ_ಠ");
+                    break;
+                case "eyeroll:":
+                    API.sendChat("/me ¬_¬");
+                    break;
+                case "yuno:":
+                    API.sendChat("/me ლ(ಥ益ಥლ");
+                    break;
+            }
+            mubBot.misc.ready = false;
+            setTimeout(function(){ mubBot.misc.ready = true; }, mubBot.settings.cooldown * 1000);
+        }
     }else{
 
         // swearing
@@ -665,25 +683,6 @@ botMethods.chatEvent = function(data){
         }
 
     }
-};
-botMethods.chatEvent = function(data){
-        emote = false; var chatCommand = "";
-        if(data.message.indexOf(":") === 0) emote = true;
-        if(emote && mubBot.misc.ready){
-            switch(chatCommand.substring(1)){
-                case "notamused:":
-                    API.sendChat("/me ಠ_ಠ");
-                    break;
-                case "eyeroll:":
-                    API.sendChat("/me ¬_¬");
-                    break;
-                case "yuno:":
-                    API.sendChat("/me ლ(ಥ益ಥლ");
-                    break;
-            }
-            mubBot.misc.ready = false;
-            setTimeout(function(){ mubBot.misc.ready = true; }, mubBot.settings.cooldown * 1000);
-        }
 };
     botMethods.loadStorage();
     console.log("Running mubBot User Shell version " + mubBot.misc.version);
